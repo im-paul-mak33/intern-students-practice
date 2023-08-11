dc608004155f5f89d4e406d178352216c6bfa904
im-paul-mak33 <souravpaul210789@gmail.com>
2023-08-11 22:27:13 +0530

    11-08-2023

---
+++ practice.js
@@ -0,0 +1,9 @@
+var greetings1 = {
+    person1: "Milaan",
+    person2: "Milaan2",
+    person3: "Milaan3",
+}
+
+person4 = "Milaan4"
+
+console.log(greetings1)
---
+++ script.js
@@ -0,0 +1,29 @@
+const greetings2 = 56
+
+const greetings3 = 69
+
+const greetings4 = greetings3 * 3
+
+console.log(greetings4)
+
+const greetings6 = "56" // String
+
+// const greetings7 = 56 // integer
+
+const greetings8 = false
+const greetings9 = !greetings8
+
+if (!greetings8) {
+    console.log(greetings9)
+} else {
+    statement = "not working"
+    console.log(statement)
+}
+
+// == != === !== > < >= <= - comparison operator
+
+// && || !
+
+
+// Start
+// tomorrow/12-08-2023 - Control Flow Statements
