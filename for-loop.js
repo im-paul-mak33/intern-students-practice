b657e54f53ad5fa855ae3dde92c3c5545db845c9
im-paul-mak33 <souravpaul210789@gmail.com>
2023-09-01 22:20:08 +0530

    added more data

--- for-loop.js
+++ for-loop.js
@@ -6,9 +6,9 @@ const cars = ["BMW", "Volvo", "Saab", "Ford"]
 
 */
 
-const cars = ["BMW", "Volvo", "Saab", "Ford"]
+const cars = ["BMW", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford", "Volvo", "Saab", "Ford"]
 
-for (let i = 0, len = cars.length, text = ""; i < len; i++) {
+for (let i = 0, len = cars.length, text = ""; i < len; i--) {
     allCars = text + cars[i];
     console.log(allCars)
 }
