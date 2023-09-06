8beff993ccd1560b7fbf78d7ef058b98f6d0c37f
im-paul-mak33 <souravpaul210789@gmail.com>
2023-09-07 00:31:15 +0530

    task solution

--- task5Sept2023.js
+++ task5Sept2023.js
@@ -16,4 +16,56 @@ Q.3
 Write a JavaScript function to get the first element of an array. Passing the parameter 'n' will return the first 'n' elements
 of the array.
 const data = [[7, 9, 0, -2]]
+
 */
+
+// Ankur
+const data = ['w3resource', 'w3resource', 'w3resource']
+const data1 = [1, 2, 4, 0]
+var cloneData = []
+var cloneData1 = []
+
+for (var i = 0; i < data.length; i++) {
+    cloneData.push(data[i]);
+}
+
+for (var i = 0; i < data1.length; i++) {
+    cloneData1.push(data1[i]);
+}
+
+// Same Array every step
+// cloneData = [1]
+// cloneData = [1, 2]
+// cloneData = [1, 2, 3, 4]
+
+console.log(cloneData)
+console.log(cloneData1)
+
+// Mine
+const cloneArray = slicingArray = (inputArray) => {
+    return inputArray.slice(0)
+}
+
+// Different Array every step
+// cloneData = [1]
+// cloneData = [1, 2]
+// cloneData = [1, 2, 3, 4]
+
+console.log(cloneArray(data))
+console.log(cloneArray(data1))
+
+
+// 3 rd question
+function getFirstElements(arr, n = 1) {
+    if (!Array.isArray(arr)) {
+        return [];
+    }
+    if (n <= 0) {
+        return [];
+    }
+    return arr.slice(0,n);
+}
+const pataNahi = [[7, 9, 0, -2], [2, 4, 1, -4]];
+const firstElement = getFirstElements(pataNahi[0]);
+
+console.log(firstElement)
