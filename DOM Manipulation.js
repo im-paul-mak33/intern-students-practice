fa5c11eda56182b8bd4f68586f4395760936bda1
im-paul-mak33 <souravpaul210789@gmail.com>
2023-09-08 23:21:51 +0530

    DOM Manipulation

--- DOM Manipulation.js
+++ DOM Manipulation.js
@@ -1,2 +1,5 @@
 const headerChange = document.querySelector(".header");
 headerChange.textContent = 'Welcome to Shop'
+
+const bannerTextManipulate = document.getElementById("bannerText")
+bannerTextManipulate.innerText = "Welcome to our shop. Anything you buy from here helps out the Gundas in Andaman. We take everything from the profit."
