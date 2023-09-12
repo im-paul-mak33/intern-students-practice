52888a39936fe852b90ee01b96959661548888ea
im-paul-mak33 <souravpaul210789@gmail.com>
2023-09-12 22:26:11 +0530

    minimum concepts to know before learning react

---
+++ React-Essentials.js
@@ -0,0 +1,14 @@
+
+
+const react_essentials = {
+    first: 'Arrow Functions',
+    second: 'Ternary Operators',
+    third: 'Objects',
+    fourth: 'Map, Filter, Reduce -> Array Methods',
+    fifth: 'Async-Await, Promises, Fetch -> Data Fetching in Javascript',
+    sixth: 'DOM',
+    seventh: 'Import/Export Notation',
+    eight: `Conditional's`,
+    ninth: 'Optional Chaining',
+    tenth: 'Template Literals',
+}
