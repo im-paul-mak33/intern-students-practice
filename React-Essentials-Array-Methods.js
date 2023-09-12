b8f83d375199481b1bcfae03f33e1e88a51ea9aa
im-paul-mak33 <souravpaul210789@gmail.com>
2023-09-12 22:26:43 +0530

    Array Methods - only Map done

---
+++ React-Essentials-Array-Methods.js
@@ -0,0 +1,9 @@
+// Map, Filter, Reduce -> Array Methods
+
+const bachhaLog = ['gouranga, mahesh, alien, anshul, bachha, pencil, chota bachha, kundru'];
+
+const removeText = bachhaLog.map(function (name) {
+    return name
+})
+
+console.log(removeText)
